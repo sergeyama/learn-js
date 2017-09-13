10ad448cfc23e077b62f59ef264564b01cc511a7
Sergei Malkov <sergeyama@mail.ru>
2017-09-13 21:30:43 +0300

    Chapter 5 done
    page 258

---
+++ 010 - fiat.js
@@ -0,0 +1,46 @@
+var fiat = {
+    make: "Fiat",
+    model: "500",
+    year: 1957,
+    color: "Medium Blue",
+    passengers: 2,
+    convertible: false,
+    mileage: 88000,
+    started: false,
+    fuel: 0,
+    start: function () {
+        if (this.fuel > 0) {
+            this.started = true;
+        } else {
+            alert("The car is on empty, fill up before starting!");
+        }
+    },
+    stop: function () {
+        this.started = false;
+    },
+    drive: function () {
+        if (this.started) {
+            if (this.fuel > 0) {
+                alert(this.make + " " + this.model + " goes zoom zoom!");
+                this.fuel--;
+            } else {
+                alert("Uh oh, out of fuel.");
+                this.stop();
+            }
+        } else {
+            alert("You need to start the engine first.");
+        }
+    },
+    addFuel: function (amount) {
+        this.fuel = this.fuel + amount;
+    }
+};
+
+fiat.start();
+fiat.drive();
+fiat.addFuel(2);
+fiat.start();
+fiat.drive();
+fiat.drive();
+fiat.drive();
+fiat.stop();
